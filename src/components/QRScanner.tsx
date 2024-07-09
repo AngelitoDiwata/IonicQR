94931ca1a852309f142e6f5a02c00c359014340c
Angelito Diwata <diwataangelito@gmail.com>
2024-07-09 13:33:37 +0800

    Improved list view (#2)
    Improved code validation upon scan

--- src/components/QRScanner.tsx
+++ src/components/QRScanner.tsx
@@ -1,6 +1,11 @@
+import { IonAlert } from "@ionic/react";
+import { useEffect, useState } from "react";
 import { useZxing } from "react-zxing";
 
-export default function QRScanner({ handleScan, paused }: any) {
+export default function QRScanner({ handleScan, paused, invalidScan }: any) {
+
+    const [isAlertOpen, setIsAlertOpen] = useState(false);
+    const [scanMsg, setScanMsg] = useState("");
 
     const { ref } = useZxing({
         onDecodeResult(result) {
@@ -11,9 +16,23 @@ export default function QRScanner({ handleScan, paused }: any) {
         paused
     });
 
+    useEffect(() => {
+        if (invalidScan) {
+            setIsAlertOpen(true)
+            setScanMsg('Please scan a valid QR code')
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [invalidScan])
+
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '40px', marginBottom: '40px' }}>
             <video ref={ref} style={{ width: '80%', border: '3px dashed white', borderRadius: '10px' }} />
+            <IonAlert
+                isOpen={isAlertOpen}
+                header={scanMsg}
+                buttons={['Close']}
+                onDidDismiss={() => setIsAlertOpen(false)}
+            ></IonAlert>
         </div>
 
     )
--- src/pages/Count.tsx
+++ src/pages/Count.tsx
@@ -1,4 +1,4 @@
-import { IonAlert, IonContent, IonHeader, IonIcon, IonItem, IonList, IonPage, IonText, IonToolbar } from '@ionic/react';
+import { IonAlert, IonButtons, IonCol, IonContent, IonGrid, IonIcon, IonItem, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import { useStorage } from '../hooks/useStorage';
 import './Home.css';
 import QRScanner from '../components/QRScanner';
@@ -11,13 +11,21 @@ const Count = ({ onBack, location, data, camPaused }: any) => {
     const [currentData, setCurrentData] = useState(data)
     const [isAlertOpen, setIsAlertOpen] = useState(false)
     const [scanMsg, setScanMsg] = useState('')
+    const [invalidScan, setInvalidScan] = useState(false)
 
     const pushData = async (res: any) => {
-        if (!isAlertOpen) {
+        if (!checkValidQRCode(res.getText())) {
+            setInvalidScan(true)
+        }
+        else if (!isAlertOpen) {
             setScanMsg(res.getText());
             setIsAlertOpen(true)
             setCurrentData(await addData(res.getText(), location));
         }
+    }
+
+    const checkValidQRCode = (code: string) => {
+        return code.split(',').length === 4
     }
 
     useEffect(() => {
@@ -28,21 +36,35 @@ const Count = ({ onBack, location, data, camPaused }: any) => {
 
     return (
         <IonPage>
-            <IonHeader>
-                <IonToolbar color="primary">
-                    <IonIcon size='large' onClick={onBack} icon={arrowBack}></IonIcon>
-                </IonToolbar>
-            </IonHeader>
+            <IonToolbar>
+                <IonButtons onClick={onBack} slot="start">
+                    <IonIcon className="ion-padding" size="medium" icon={arrowBack}></IonIcon>
+                </IonButtons>
+                <IonTitle>Location/Lot#: {location}</IonTitle>
+            </IonToolbar>
             <IonContent className="ion-padding">
-                <IonText>Location/Lot No: {location}</IonText>
                 <IonItem>
-                    <QRScanner paused={camPaused} handleScan={pushData} />
+                    <QRScanner invalidScan={invalidScan} paused={camPaused} handleScan={pushData} />
                 </IonItem>
-                <IonList>
+
+                <IonGrid>
+
+                    <IonRow style={{ backgroundColor: 'white', color: 'black', borderRadius: '5px', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)' }}>
+                        {
+                            ['PRDCODE', 'UNIT', 'LOT#', 'QTY'].map((item: any, key: any) => (<IonCol style={{ fontWeight: 'bold' }} key={key}>{item}</IonCol>))
+                        }
+                    </IonRow>
                     {
-                        currentData[location] && currentData[location].map((todo: any, key: any) => <IonItem key={key}>{todo.task}</IonItem>)
+                        currentData[location] && currentData[location].map((todo: any, key: any) => (
+                            <IonRow key={key}>
+                                {
+                                    todo.task.split(",").map((item: any, key2: any) => <IonCol style={{ paddingTop: '10px', paddingBottom: '10px' }} key={key2}>{item}</IonCol>)
+                                }
+                            </IonRow>
+                        ))
                     }
-                </IonList>
+
+                </IonGrid>
             </IonContent>
             <IonAlert
                 isOpen={isAlertOpen}
